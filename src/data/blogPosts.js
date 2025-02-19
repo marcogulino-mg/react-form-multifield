17b5f5bd4bf2c71b79591d623dbdea241123f345
marco-gulino <marcogulino98@gmail.com>
2025-02-19 15:06:32 +0100

    [feat] create the array of objects of blog posts

---
+++ src/data/blogPosts.js
@@ -0,0 +1,39 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: "Introduzione a JavaScript",
+    description:
+      "Una guida per principianti su JavaScript e le sue funzionalità di base.",
+    author: "Mario Rossi",
+  },
+  {
+    id: 2,
+    title: "Come migliorare la SEO del tuo sito",
+    description:
+      "Strategie e consigli per ottimizzare il tuo sito web per i motori di ricerca.",
+    author: "Luca Bianchi",
+  },
+  {
+    id: 3,
+    title: "I vantaggi del cloud computing",
+    description:
+      "Scopri perché sempre più aziende stanno migrando verso il cloud.",
+    author: "Giulia Verdi",
+  },
+  {
+    id: 4,
+    title: "Guida completa a React",
+    description:
+      "Tutto quello che devi sapere per iniziare a sviluppare con React.",
+    author: "Federico Neri",
+  },
+  {
+    id: 5,
+    title: "Consigli per scrivere codice pulito",
+    description:
+      "Best practices per mantenere il tuo codice leggibile e manutenibile.",
+    author: "Elena Rizzi",
+  },
+];
+
+export default blogPosts;
