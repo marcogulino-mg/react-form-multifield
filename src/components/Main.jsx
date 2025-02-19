784645c5fe4ad219817daf573c9fc37b72cd6afe
marco-gulino <marcogulino98@gmail.com>
2025-02-19 16:00:09 +0100

    [feat] add form in AddArticle.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
 // IMPORT ArticleList.jsx
 import Articlelist from "./subcomponents/ArticleLists";
+// IMPORT AddArticle.jsx
+import Addarticle from "./subcomponents/AddArticle";
 
 export default function Main() {
   return (
     <main>
       <h2>Lista articoli</h2>
+      <Addarticle />
       <Articlelist />
     </main>
   );
