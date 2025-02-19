398641d1b86f0ec0c061993b09afd20b9da48144
marco-gulino <marcogulino98@gmail.com>
2025-02-19 14:50:36 +0100

    [feat] add macroblocks components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+// IMPORT Macro Blocks
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 }
 
 export default App;
